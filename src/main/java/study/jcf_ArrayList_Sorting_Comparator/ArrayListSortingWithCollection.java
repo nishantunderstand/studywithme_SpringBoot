b15cb84df1519712e0f95207e233457312fe12a9
Nishant Raj <nishant.read@gmail.com>
2026-09-13 18:47:40 +0530

    Sunday, September 13, 2026 6:47:38 PM

---
+++ src/main/java/study/jcf_ArrayList_Sorting_Comparator/ArrayListSortingWithCollection.java
@@ -0,0 +1,16 @@
+package study.jcf_ArrayList_Sorting_Comparator;
+
+import java.util.*;
+
+class ArrayListSortingWithCollection {
+    public static void main(String[] args) {
+        List<Integer> al1 = List.of(11, 99, 88, 22, 33, 44);
+        // Arrays.sort(al1);
+        // Collections.sort(al1); // java.lang.UnsupportedOperationException
+        Collections.sort(new ArrayList<>(al1)); // Why it is working ??
+        System.out.println(al1);
+
+
+
+    }
+}
