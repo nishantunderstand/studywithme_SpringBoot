d1441f6ca22fe83a16949b9fae7505e58839b915
Nishant Raj <nishant.read@gmail.com>
2026-09-15 22:36:49 +0530

    Tuesday, September 15, 2026 10:36:47 PM

---
+++ src/main/java/com/studywithme/entity/StudentEntity.java
@@ -0,0 +1,15 @@
+package com.studywithme.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="students")
+public class StudentEntity {
+}
