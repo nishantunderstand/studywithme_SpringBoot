5f99ced1e990cd146b5425007d05e1d1d8f24492
Nishant Raj <nishant.read@gmail.com>
2026-09-15 15:49:42 +0530

    Tuesday, September 15, 2026 3:28:29 PM

--- src/main/java/com/studywithme/controller/StudentController2.java
+++ src/main/java/com/studywithme/controller/StudentController2.java
@@ -1,23 +1,24 @@
 package com.studywithme.controller;
 
 import com.studywithme.dto.StudentResponseDTO;
+import com.studywithme.repository.StudentRepository;
 import com.studywithme.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/students1")
-public class StudentController {
+@RequestMapping("/api/students")
+public class StudentController2 {
     private final StudentService studentService;
+
     @GetMapping("/{id}")
     public ResponseEntity<StudentResponseDTO> getStudentById(@PathVariable Long id){
         return ResponseEntity.ok(studentService.getStudentById(id));
     }
+
+
 }
-
-
